2d6fb6b0f528f1669153b2ba3d0604e09b46af55
C-Dongbo <chlehdrms3@gmail.com>
2021-02-18 20:13:26 +0900

    cluster news processing

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import configparser
 import requests
 import re
+import json
 
 from bs4 import BeautifulSoup
 
@@ -18,13 +19,39 @@ class naver():
         news_info_list = soup.select('#main_pack > section > div.api_subject_bx > div.group_news > ul > li')
         return news_info_list
     
-    def get_page_info(self):
-        page_info_list = self.soup.select_one('#main_pack > div.api_sc_page_wrap > div.sc_page > div.sc_page_inner')
-        page_list = ['https://search.naver.com/search.naver'+data['href'] for data in page_info_list.find_all('a')]
-        return page_list        
+    def get_page_info(self, soup=None):
+        if soup == None:
+            page_info_list = self.soup.select_one('#main_pack > div.api_sc_page_wrap > div.sc_page > div.sc_page_inner')
+            page_list = ['https://search.naver.com/search.naver'+data['href'] for data in page_info_list.find_all('a')]
+            return page_list
+        else:
+            page_info_list = soup.select_one('#main_pack > div.api_sc_page_wrap > div.sc_page > div.sc_page_inner')
+            page_list = ['https://search.naver.com/search.naver'+data['href'] for data in page_info_list.find_all('a')]
+            return page_list
+            
 
-    def get_cluster_news_info(self):
-        return
+    def get_cluster_news_info(self, url):
+        res = requests.get(url)
+        soup = BeautifulSoup(res.content, 'html.parser')
+        page_list = self.get_page_info(soup)
+
+        results = []
+        first_news_flag = True
+
+        for page_url in page_list:
+            res = requests.get(page_url)
+            soup = BeautifulSoup(res.content, 'html.parser')
+            news_info_list = self.get_news_html(soup)
+            for news_info in news_info_list:
+                if first_news_flag and first_page_flag:
+                    first_news_flag = False
+                    continue
+                title = news_info.select_one('div.news_wrap.api_ani_send > div.news_area > a').text
+                desc = news_info.select_one('div.news_wrap.api_ani_send > div.news_area > div.news_dsc > div.dsc_wrap > a').text
+                news_url = news_info.find("a")["href"]
+                results.append({"title": title, "desc":desc, "url":news_url})
+
+        return results
 
     def get_news_info(self):
         page_list = self.get_page_info()
@@ -40,15 +67,23 @@ class naver():
                 desc = news_info.select_one('div.news_wrap.api_ani_send > div.news_area > div.news_dsc > div.dsc_wrap > a').text
                 news_url = news_info.find("a")["href"]
                 cluster = news_info.select_one('a.news_more')  # 숫자만 빼내야댐
-    #            if cluster == None:
-    #
-    #            print(cluster)
-                cluster_news_url = 'https://search.naver.com/search.naver?where=news&query={}&sm=tab_opt&sort=0&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid={}&nso=&mynews=0&refresh_start=0&related=1'.format(self.query, "test")
-                results[self.id] = {"title":title, "desc":desc, "url":news_url}
+                if cluster != None:
+                    cluster_str = str(cluster)
+                    idx = cluster_str.find("news_submit_related_option('") + len("news_submit_related_option('")
+                    num = cluster_str[idx:idx+13]
+                    cluster_news_url = 'https://search.naver.com/search.naver?where=news&query={}&sm=tab_opt&sort=0&photo=0&field=0&reporter_article=&pd=0&ds=&de=&docid={}&nso=&mynews=0&refresh_start=0&related=1'.format(self.query, num)
+                    cluster_news_results = self.get_cluster_news_info(cluster_news_url)
+                results[self.id] = {"title":title, "desc":desc, "url":news_url, "cluster_news_list":cluster_news_results}
                 results_list.append(results)
         return results_list
 
         
 if __name__ == '__main__':
     parser = naver('삼성전자')
-    print(parser.get_news_info())
+    news = parser.get_news_info()
+
+    with open("news.json", "w", encoding='utf8') as json_file:
+        json.dump(news, json_file)
+
+
+
