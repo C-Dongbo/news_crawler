2a8701876a053e0f0f2b2b3ca01accc68646c9cf
C-Dongbo <chlehdrms3@gmail.com>
2021-03-04 09:25:48 +0900

    update

--- logging_test.py
+++ logging_test.py
@@ -1,20 +1,24 @@
 import logging
+import argparse
 from logging.config import fileConfig
 
+parser = argparse.ArgumentParser(description='Logging')
+parser.add_argument('--logger_name', type=str, default='root')
 
 def main(logger_name):
 
-    if logger_name not in ["root", "logstashOnly", "consoleOnly"]:
+    if logger_name not in ["root", "logstash_only", "console_only"]:
         raise Exception("logger_name(" + logger_name + ") does not exist")
 
     fileConfig('logging.conf', disable_existing_loggers=True)
     logger = logging.getLogger(logger_name)
-    logger.info("App Start")
+    logger.info("Start")
     try:
         return 1 / 0 ## generate exception
     except Exception as e:
         logger.error(e)
-    logger.info("App End")
+    logger.info("End")
 
 if __name__ == '__main__':
-    main()
+    args = parser.parse_args()
+    main(args.logger_name)
